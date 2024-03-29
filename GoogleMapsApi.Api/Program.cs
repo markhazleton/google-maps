a10768c9dbca53ebd33c0874ef236b80860cd698
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-03-29 14:34:02 -0500

    Swagger Always On

--- GoogleMapsApi.Api/Program.cs
+++ GoogleMapsApi.Api/Program.cs
@@ -32,13 +32,8 @@ builder.Services.AddSingleton<IMapsService, GoogleMapsServiceApi>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseMiddleware<LoggingMiddleware>();
 
